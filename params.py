6c707bf688a08a197977159a09015928c0a453e4
SHostetter <aasere@gmail.com>
2018-12-13 13:28:17 -0500

    adds latest version of LION->CLION code

---
+++ params.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+
+__author__ = 'SHostetter'
+# set up base globals
+DB_HOST =  # database host
+DB_NAME =  # Database
+WORKING_SCHEMA = 'public'  # 'working'
+FINAL_SCHEMA = 'public'
+LION = 'lion'  
+NODE = 'node'  '
+RPL = 'tbl_rpl'
+RPL_TXT = 'RPL.txt'
+VERSION = '18d'  # '15b'
+PRECINCTS = 'districts_police_precincts'
+BOROUGHS = 'districts_boroughs'
+HIGHWAYS = True
+SRID = 2263
+
+FOLDER = r'C:\Users\SHostetter\Desktop\GIT\CLION2\DATA\18D'
+
+# global dictionaries
+def st_name_factory():
+    return [set(), 0]
+nodeStreetNames = defaultdict(st_name_factory)  # {node: [{street names}, masterid}
+nodeIsIntersection = {}  # {node: True or False}
+nodeNextSteps = defaultdict(lambda: defaultdict(set))  # {node: {street: fromNode, toNode}}
+segmentBlocks = {}  # {segmentID: fromMaster, toMaster}
+nodeMaster = {}  # {node: masterid}
+masterNodes = defaultdict(list)  # {masterid: [nodeid, nodeid, ...]}
+clusterIntersections = defaultdict(st_name_factory)  # {sorted-street-names: [set([nodes]), masterID]}
+mfts = []
+coordFromMaster = {}  # {master: [x,y]
+# minor datastores - can be deleted after use?
+streetSet = []
+mft1Dict = defaultdict(list)  # mft: [segmentid, segmentid]
+altGraph = defaultdict(list)
